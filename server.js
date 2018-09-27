4655c77227115967b46193816d6bce586c6975d5
James Albert Phillips V <jamesalbertphillipsv@jamess-air.attlocal.net>
2018-09-26 21:07:34 -0700

    connect to heroku

--- server.js
+++ server.js
@@ -91,6 +91,14 @@ app.get("/comments", function(req, res) {
   })
 });
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
